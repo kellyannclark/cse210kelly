e0d6f7fdffa734f7b3d9d9e10fae8eb455b50bd6
kellyannclark <thefaithseed@gmail.com>
2023-01-16 15:06:53 -0700

    changes

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        Console.WriteLine("Welcome to your journal app.");
+        Console.WriteLine("Please choose from the following menu:");
+        Console.WriteLine("1. Write");
+        Console.WriteLine("2. Display");
+        Console.WriteLine("3. Load");
+        Console.WriteLine("4. Save");
+        Console.WriteLine("5. Quit");
+        Console.Write(">");
+        
+        string menu_option = Console.ReadLine();
+
+
+
+
+
+
     }
 }
