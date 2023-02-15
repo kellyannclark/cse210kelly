35bdac2ac7b67575a5a7cda592e12795e4e23506
kellyannclark <thefaithseed@gmail.com>
2023-02-15 10:21:59 -0700

    changes

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -2,8 +2,32 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Develop03 World!");
+        // Create a new instance of the ScriptureCollection class
+        ScriptureCollection scriptureCollection = new ScriptureCollection();
+
+        string user_go = "";
+
+        Console.WriteLine("Welcome to your scripture memorizer!");
+        Console.WriteLine("Type 'go' to have the random selector choose either a single scripture or multiple verses.");
+        Console.WriteLine("The program will quit when all the words disappear.");
+
+        user_go = Console.ReadLine();
+
+        while (user_go == "go")
+        {
+            // Call the GetRandomScripture method to get a random scripture
+            Scripture randomScripture = scriptureCollection.GetRandomScripture();
+
+            // Display the reference and text of the random scripture
+            Console.WriteLine("Reference: " + randomScripture.Reference);
+            Console.WriteLine("Text: " + randomScripture.Text);
+
+            // Prompt the user to continue or quit
+            Console.WriteLine("Press 'go' to see another random scripture, or any other key to quit.");
+            user_go = Console.ReadLine();
+        }
     }
 }
+
---
+++ prove/Develop03/ScriptureCollection.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+public class ScriptureCollection
+{
+    private List<Scripture> _scriptures = new List<Scripture>();
+
+    public ScriptureCollection()
+    {
+        // Constructor for the ScriptureCollection class
+        // Loads the scripture references and verses from a text file called "scripture.txt"
+        // and adds them to the _scriptures list
+
+        // Read the contents of the file into an array of strings
+        string[] lines = File.ReadAllLines("scripture.txt");
+
+        // Loop through each line of the file
+        foreach (string line in lines)
+        {
+            // Split the line into two parts: the reference and the text
+            string[] parts = line.Split('\t');
+            string reference = parts[0];
+            string firstVerse = parts[1];
+            string secondVerse = parts[2];
+
+            // Create a new Scripture object with the reference and text
+            Scripture scripture = new Scripture(reference, firstVerse, secondVerse);
+
+            // Add the new Scripture object to the _scriptures list
+            _scriptures.Add(scripture);
+        }
+    }
+
+    public Scripture GetRandomScripture()
+    {
+        // Method that returns a random scripture from the collection
+
+        // Create a new Random object
+        Random rand = new Random();
+
+        // Generate a random index between 0 and the number of scriptures in the collection
+        int index = rand.Next(_scriptures.Count);
+
+        // Return the scripture at the randomly generated index
+        return _scriptures[index];
+    }
+}
